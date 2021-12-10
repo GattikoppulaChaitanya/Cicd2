c1a0589fd6c8d47fcbc9d2184c3521e322822611
ChaitanyaGattikoppul <chaitanya.gattikoppula@ratnaglobaltech.com>
2021-12-10 11:25:32 +0530

    expermantal

--- app/src/main/java/com/example/cicd2/MainActivity.kt
+++ app/src/main/java/com/example/cicd2/MainActivity.kt
@@ -9,6 +9,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         println("ANVCG")
         println("rkjvbkjfevbfkjdvjdfk")
-        dgdfgdfbfdb
+        println("Expermantal")
+
     }
 }
